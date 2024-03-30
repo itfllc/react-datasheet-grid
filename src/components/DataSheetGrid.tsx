f4eb36c1b66a5db979517e32ae84680cde2be46e
yuki-yotsui0119 <yotsui.yuki@itfllc.co.jp>
2024-03-30 16:25:28 +0900

    テキストカラムのIME対応

--- src/components/DataSheetGrid.tsx
+++ src/components/DataSheetGrid.tsx
@@ -1467,9 +1467,7 @@ export const DataSheetGrid = React.memo(
               lastEditingCellRef.current = activeCell
               setSelectionCell(null)
               // NOTE: 非編集状態でキー入力した際、IMEが有効になっていても半角英数が入力されてしまうので、focusイベントを遅延発火させる
-              setTimeout(() => {
                 setEditing(true)
-              })
               scrollTo(activeCell)
             }
           } else if (['Backspace', 'Delete'].includes(event.key)) {
